7eb0c56c5a8afff5426b9049da936401c5371359
tanveer-developer123 <youloos477@gmail.com>
2025-12-28 08:27:54 +0500

    inital commit
    start a data types in c langauge

---
+++ Week 1/Data_Types/data_types.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // integear mean that all is Numbers
+    int a, b, c, d, e;
+
+    a = 10;
+    b= 20;
+    c = 30;
+    d = 40;
+    e = a  + b + c + d;
+    printf("%i" ,e);
+    
+    return 0;
+}
---
+++ Week 1/Firest_code.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){ // everytime program should have main function
+
+    printf("Hell Wordl!");
+    
+    return 0; // return keyword is must be used like 
+}
