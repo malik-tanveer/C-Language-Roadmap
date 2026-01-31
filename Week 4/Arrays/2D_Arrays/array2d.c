b7d57281ce15ee880b066d8c155171bba4be1f4c
tanveer-developer123 <youloos477@gmail.com>
2026-01-31 07:59:45 +0500

    2D array completed

--- Week 4/Arrays/2D_Arrays/array2d.c
+++ Week 4/Arrays/2D_Arrays/array2d.c
@@ -10,16 +10,43 @@ int main()
     // {4, 5, 6}
     // };
 
-    // Method 2: Without row size
+    // Method 2: Without row size Evry time you write a Column Vlaues
     // int arr[][3] = {
     // {1, 2, 3},
     // {4, 5, 6}
     // };
 
-    int Arr[5][5] = {
-        {0, 1, 2, 3, 4},
-        {10, 20, 30, 40, 50}};
+    // Simple 2d array print using pritnf
+    int Arr[][5] = {
+        {20},
+        {10, 20},
+        {10, 20,30},
+        {10, 20,30,40},
+        {10, 20,30,40,50},
+        {20, 1, 2, 3, 4},
+    };
+    
+    
+    printf("%d\n", Arr[0][0]);
+    printf("%d %d \n", Arr[1][0], Arr[1][1]);
+    printf("%d %d %d\n", Arr[2][0], Arr[2][1], Arr[2][2]);
+    printf("%d %d %d %d\n", Arr[3][0], Arr[3][1], Arr[3][2], Arr[3][3]);
+    printf("%d %d %d %d %d\n", Arr[4][0], Arr[4][1], Arr[4][2], Arr[4][3], Arr[4][4]);
+    printf("%d %d %d %d %d\n", Arr[5][0], Arr[5][1], Arr[5][2], Arr[5][3], Arr[5][4]);
+    
 
-    printf("2D Array : %d, %d\n", Arr[2][1]);
+    // Simple 2d array print using Loop
+int rows = sizeof(Arr) / sizeof(Arr[0]);
+int cols = sizeof(Arr[0]) / sizeof(Arr[0][0]);
+printf("Rows = %d\n", rows);
+printf("cols = %d\n", cols); 
+   for(int i =0;i<rows;i++)
+    {
+        for(int j=0;j<cols;j++)
+        {
+            printf("%d ", Arr[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
